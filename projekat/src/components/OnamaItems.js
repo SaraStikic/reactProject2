d74dfe4cb836b9d82569b5f67f2f3eae568b2f47
Sara Stikic <sarastikic10@gmail.com>
2021-07-02 22:47:57 +0200

    Last Commit

---
+++ projekat/src/components/OnamaItems.js
@@ -0,0 +1,17 @@
+export const OnamaItems=[
+    {
+        url: "https://freepikpsd.com/wp-content/uploads/2019/10/film-png-icon-Transparent-Images.png",
+       
+        cName: 'img'
+    },
+    {
+        url:"https://cdn4.iconfinder.com/data/icons/movie-basics/48/v-35-512.png",
+       
+        cName: 'img'
+    },
+    {
+        url:"https://webstockreview.net/images/cinema-clipart-movie-symbol.png",
+        cName: 'img' 
+    }
+   
+]
